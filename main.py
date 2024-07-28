77948ac351b9036425ec1f5e52dfd4b52004c43f
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-07-28 16:58:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from scripts import embedding_script
+from scripts import semantic_audit_script
 
 PAGES = {
     "Embedding Script": embedding_script
+    "Audit Sémantique": semantic_audit_script
 }
 
 st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+selection = st.sidebar.radio("Maillage interne", list(PAGES.keys()))
 
 page = PAGES[selection]
 page.app()
