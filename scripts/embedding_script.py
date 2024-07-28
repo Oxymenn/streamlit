faf67834fca7242930519500a5aef46430faa641
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-07-28 18:14:01 +0200

    Update embedding_script.py

--- scripts/embedding_script.py
+++ scripts/embedding_script.py
@@ -23,7 +23,7 @@ def clean_text(text):
     return ' '.join(words)
 
 def get_embedding(text, model="text-embedding-3-small"):
-    response = openai.Embedding.create(input=text, model=model)
+    response = openai.Embedding.create(input=[text], model=model)
     embedding = response['data'][0]['embedding']
     return embedding
 
