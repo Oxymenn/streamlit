1c59f8486c3d87324e459fe5cd787515aa87189a
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-07-28 23:34:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,33 +4,39 @@ from scripts import semantic_audit_script
 from scripts import cannibalisation_serp
 
 # Configuration des pages
-PAGES_MAILLAGE = {
-    "Embedding Script": embedding_script,
-    "Audit Sémantique": semantic_audit_script,
-}
-
-PAGES_SERP = {
-    "Cannibalisation SERP": cannibalisation_serp,
+PAGES = {
+    "Maillage interne": {
+        "Embedding Script": embedding_script,
+        "Audit Sémantique": semantic_audit_script,
+    },
+    "Analyse SERP": {
+        "Cannibalisation SERP": cannibalisation_serp,
+    }
 }
 
 # Titre principal
 st.sidebar.title("Scripts de Pirates")
 
+# Initialisation de la sélection
+selected_script = None
+
 # Sous-titre et choix des scripts de maillage interne
 st.sidebar.subheader("Maillage interne")
-selection_maillage = st.sidebar.radio("", list(PAGES_MAILLAGE.keys()), key="maillage")
+maillage_script = st.sidebar.radio("", list(PAGES["Maillage interne"].keys()), key="maillage", index=0)
 
 # Sous-titre et choix des scripts d'analyse SERP
 st.sidebar.subheader("Analyse SERP")
-selection_serp = st.sidebar.radio("", list(PAGES_SERP.keys()), key="serp")
+serp_script = st.sidebar.radio("", list(PAGES["Analyse SERP"].keys()), key="serp", index=0)
 
-# Affichage du script sélectionné
-if selection_maillage and not selection_serp:
-    page = PAGES_MAILLAGE[selection_maillage]
-    page.app()
-elif selection_serp and not selection_maillage:
-    page = PAGES_SERP[selection_serp]
-    page.app()
+# Déterminer quel script est sélectionné
+if maillage_script:
+    selected_script = PAGES["Maillage interne"][maillage_script]
+if serp_script:
+    selected_script = PAGES["Analyse SERP"][serp_script]
+
+# Exécuter le script sélectionné
+if selected_script:
+    selected_script.app()
 
 # Copyright
 st.sidebar.markdown("© 2024 | by PirateSEO")
