3c60932e1921dcaf99f13186909a791661ff2772
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-08-04 15:39:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,25 @@ from scripts import test_similarite
 
 # Configuration des pages
 PAGES = {
-@@ -13,7 +14,8 @@
+    "Embedding Script": embedding_script,
+    "Similarité Cosinus": similarite_cosinus,
     "Audit Maillage Interne": audit_maillage_interne,
     "Cannibalisation SERP": cannibalisation_serp,
     "Catégorisation des produits": categorisation_produit,
-    "Test Cannibalisation": test_cannibalisation
     "Test Cannibalisation": test_cannibalisation,
     "Test Similarité": test_similarite
 }
 
 # Titre principal
 st.sidebar.title("Scripts de Pirates")
+
 # Sous-titre et choix des scripts
 st.sidebar.subheader("Les scripts")
 selection = st.sidebar.radio("", list(PAGES.keys()), index=0)
+
 # Affichage du script sélectionné
 page = PAGES[selection]
 page.app()
+
 # Copyright
 st.sidebar.markdown("© 2024 | by PirateSEO")
