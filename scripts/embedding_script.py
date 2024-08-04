e6d7ce8cb28f54ac0092c919cbf947fe05e3bdee
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-08-04 21:23:35 +0200

    Update embedding_script.py

--- scripts/embedding_script.py
+++ scripts/embedding_script.py
@@ -94,7 +94,7 @@ def calculate_similarity(embeddings):
 
 # Fonction principale de l'application
 def app():
-    st.title("Analyse sémantique - Pages similaires")
+    st.title("Pages Similaires Sémantiquement - Woocommerce (Shoptimizer)")
     uploaded_file = st.file_uploader("Importer un fichier CSV ou Excel contenant des URLs", type=["csv", "xlsx"])
 
     if uploaded_file is not None:
