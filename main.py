4362654f29ad635c73203a6e16627a46ba462fca
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-08-04 17:16:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from scripts import embedding_script
 from scripts import similarite_cosinus
 from scripts import audit_maillage_interne
 from scripts import cannibalisation_serp
@@ -9,7 +8,6 @@ from scripts import test_similarite
 
 # Configuration des pages
 PAGES = {
-    "Embedding Script": embedding_script,
     "Similarité Cosinus": similarite_cosinus,
     "Audit Maillage Interne": audit_maillage_interne,
     "Cannibalisation SERP": cannibalisation_serp,
