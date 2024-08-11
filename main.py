80dc64b5a1dcb4a66ca76c973f435db74d8ca872
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-08-11 21:08:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from scripts import analyse_proposition_maillage
 from scripts import proposition_maillage
-from scripts import similarite_cosinus
 from scripts import cannibalisation_serp_payant
 from scripts import cannibalisation_serp_gratuit
 from scripts import images_bulk
@@ -11,8 +10,7 @@ from scripts import post_article_wp
 PAGES = {
     "Analyse + Proposition Maillage": analyse_proposition_maillage,
     "Proposition Maillage": proposition_maillage,
-    "Similarité Cosinus": similarite_cosinus,
-        "Analyse Cannibalisation SERP (2€30 pour 1000 mots)": cannibalisation_serp_payant,
+    "Analyse Cannibalisation SERP (2€30 pour 1000 mots)": cannibalisation_serp_payant,
     "Analyse Cannibalisation SERP (gratuit)": cannibalisation_serp_gratuit,
     "Images Bulk": images_bulk,
     "Post Article WP": post_article_wp
