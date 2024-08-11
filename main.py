8daa5f6a2039e91164886f04ba8b2450daa059d2
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-08-11 21:23:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from scripts import analyse_proposition_maillage
 from scripts import proposition_maillage
+from scripts import tri_keywords
 from scripts import cannibalisation_serp_payant
 from scripts import cannibalisation_serp_gratuit
 from scripts import images_bulk
 from scripts import post_article_wp
 
+
 # Configuration des pages
 PAGES = {
     "Analyse + Proposition Maillage": analyse_proposition_maillage,
     "Proposition Maillage": proposition_maillage,
+    "Tri + Nettoyage de mots-clés": tri_keywords,
     "Analyse Cannibalisation SERP (2€30 pour 1000 mots)": cannibalisation_serp_payant,
     "Analyse Cannibalisation SERP (gratuit)": cannibalisation_serp_gratuit,
     "Images Bulk": images_bulk,
