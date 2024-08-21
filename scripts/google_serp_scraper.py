0b493eaf0fc294f508e2939875c46c6cae99ec9b
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-08-22 01:17:39 +0200

    Update google_serp_scraper.py

--- scripts/google_serp_scraper.py
+++ scripts/google_serp_scraper.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
@@ -34,11 +36,12 @@ def app():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')  # Optional: may solve some issues
     chrome_options.add_argument(f"user-agent={user_agent}")
     chrome_options.add_argument("--window-size=1366,768")
 
-    # Start WebDriver
-    wd = webdriver.Chrome(options=chrome_options)
+    # Start WebDriver with ChromeDriverManager
+    wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     # Stopwords directly embedded in the code
     stopwords = {
