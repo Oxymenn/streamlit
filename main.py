ab6321dae140804e87c58e946e01eab648c4aec4
Oxymenn <167655232+Oxymenn@users.noreply.github.com>
2024-10-16 11:29:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ from scripts import google_serp_scraper
 from scripts import testmaillage
 from scripts import extraction_serp_dataforseo
 
-
-
 # Configuration des pages
 PAGES = {
     "Analyse + Proposition Maillage": analyse_proposition_maillage,
     "Proposition Maillage": proposition_maillage,
+    "Extraction SERP - DataForSEO": extraction_serp_dataforseo,
     "Tri + Nettoyage de mots-clés": tri_keywords,
     "Analyse Cannibalisation SERP (2€30 pour 1000 mots)": cannibalisation_serp_payant,
     "Analyse Cannibalisation SERP (gratuit)": cannibalisation_serp_gratuit,
@@ -24,9 +23,7 @@ PAGES = {
     "Post Article WP": post_article_wp,
     "Audit SEO On-page": audit_on_page,
     "Google SERP Scraper": google_serp_scraper,
-    "Maillage TEST": testmaillage,
-    "Extraction SERP - DataForSEO": extraction_serp_dataforseo
-
+    "Maillage TEST": testmaillage
 }
 
 # Titre principal
@@ -37,8 +34,11 @@ st.sidebar.subheader("Les scripts")
 selection = st.sidebar.radio("", list(PAGES.keys()), index=0)
 
 # Affichage du script sélectionné
-page = PAGES[selection]
-page.app()
+if hasattr(PAGES[selection], "app"):
+    page = PAGES[selection]
+    page.app()
+else:
+    st.error(f"Le module {selection} n'a pas de fonction 'app'. Veuillez vérifier le fichier correspondant.")
 
 # Copyright
 st.sidebar.markdown("© 2024 | by PirateSEO")
